c135f3bef256c8ada19d20ad0c6c523db13d2771
luiz-ortega <luizngust@hotmail.com>
2020-03-26 01:08:04 -0300

    Node console.log testing

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,35 +1,14 @@
-// Desafio FizzBuzz
+const { expect } = require('chai');
+const { exec } = require('child_process');
 
-// Escreva uma lib que receba um número e:
+const btcConverter = './src/main.js';
 
-// Se o número for divisível por 3, no lugar do número escreva 'Fizz' - X
-// Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
-// Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' -X
-// Se não for múltiplo de NamedNodeMap, retorna o número
-
-import { expect } from 'chai';
-import FizzBuzz from '../src/main';
-
-describe('Main', () => {
-  it('should return `Fizz` when multiple of 3', () => {
-    expect(FizzBuzz(3)).to.be.equal('Fizz');
-    expect(FizzBuzz(6)).to.be.equal('Fizz');
-  });
-
-  it('should return `Buzz` when multiple of 5', () => {
-    expect(FizzBuzz(5)).of.be.equal('Buzz');
-    expect(FizzBuzz(25)).of.be.equal('Buzz');
-  });
-
-  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
-    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
-  });
-
-  it('should return the number when not multiple', () => {
-    expect(FizzBuzz(7)).to.be.equal(7);
-  });
-
-  it('should return 0 if number is 0', () => {
-    expect(FizzBuzz(0)).to.be.equal(0);
+describe('main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World');
+      done();
+    });
   });
 });
