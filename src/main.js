6bb6e46bf8e4d75fef1a60e935cc5c19d9836950
luiz-ortega <luizngust@hotmail.com>
2020-03-29 17:59:09 -0300

    Options for currency and amount. Config for build command updated.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 
 const program = require('commander');
 const pkg = require('../package.json');
+const convertBTC = require('./ConvertBTC');
 
 program
   .version(pkg.version)
   .description('Convert Bitcoin to any currency defined')
+  .option('-C, --currency <currency>', 'Currency to be converted. (Defaulted: USD)')
+  .option('-A, --amount <amount>', 'Value bitcoin to be convert. (Defaulted: 1)')
   .parse(process.argv);
+
+
+console.log(convertBTC(program.currency, program.amount));
