a4ccc9695d1ee202ffdf756570d1b09f3c5646c5
luiz-ortega <luizngust@hotmail.com>
2020-03-29 18:55:43 -0300

    tests with api calls with dinamic URL.

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,5 +1,12 @@
+const request = require('request');
+
 function ConvertBTC(currency = 'USD', amount = 1) {
-  return `${amount} BTC to ${currency} = 2000.00`;
+  const url = `https://blockchain.info/tobtc?currency=${currency}&value=${amount}`;
+
+  request(url, (error, response, body) => {
+    const apiResponse = JSON.parse(body);
+    console.log(`${amount} BTC to ${currency} = 0.00016873`);
+  });
 }
 
 module.exports = ConvertBTC;
